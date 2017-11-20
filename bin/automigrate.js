ef49296f787bcf0c4f28f9725b5be1bcdbe714a7
KealJones <kealjones@gmail.com>
2017-11-20 09:50:45 -0700

    remove comment from json as not using package that can parse that and fixed loopback table migration logger

--- bin/automigrate.js
+++ bin/automigrate.js
@@ -4,6 +4,24 @@ var app = require(path.resolve(__dirname, "../server/server"));
 var ds = app.datasources.accountDS;
 ds.automigrate("Account", function(err) {
   if (err) throw err;
+  /*
+  dummydata.json made with:
+  https://www.json-generator.com
+  [
+    '{{repeat(100)}}',
+    {
+      email: '{{email()}}',
+      name: '{{firstName()}} {{surname()}}',
+      role: '{{random("user", "artist")}}',
+      location: {
+          lat: '{{floating(23, 48)}}',
+          lng: '{{floating(-123, -65)}}'
+        },
+      createdAt: '{{date(new Date("2015-1-1"))}}',
+      lastSeenAt: '{{random(date(new Date("2016-1-1")), date(new Date("2017-11-1")), date(new Date("2017-11-1")), date(new Date()))}}'
+    }
+  ]
+  */
   var accounts = require("./dummydata.json");
   var count = accounts.length;
   accounts.forEach(function(account) {
@@ -23,7 +41,7 @@ var lbTables = ["User", "AccessToken", "ACL", "RoleMapping", "Role"];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log(
-    "Loopback tables [" - lbTables - "] created in ",
+    "Loopback tables [" + lbTables + "] created in ",
     ds.adapter.name
   );
   ds.disconnect();
