6b57045f8c3a53e2b9e42a023a86eadf5756861f
KealJones <kealjones@gmail.com>
2017-11-20 01:32:43 -0700

    adding distance property

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -2,6 +2,10 @@ module.exports = function(app) {
   // Install a "/ping" route that returns "pong"
   app.get("/getMatches", function(req, res) {
     var Account = app.models.Account;
+    var there = app.GeoPoint({
+      lat: parseFloat(req.query.lat),
+      lng: parseFloat(req.query.lng)
+    });
     return Account.find(
       {
         where: {
@@ -15,6 +19,14 @@ module.exports = function(app) {
         }
       },
       function(err, accounts) {
+        for (let index = 0; index < accounts.length; index++) {
+          var there = new app.GeoPoint({
+            lat: accounts[index].location.lat,
+            lng: accounts[index].location.lng
+          });
+
+          accounts[index].distance = there.distanceTo(here, { type: "miles" });
+        }
         return accounts;
       }
     );
