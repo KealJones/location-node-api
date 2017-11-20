dfb390aed8e41db44f89d8eb6d9083a18094e5e7
KealJones <kealjones@gmail.com>
2017-11-20 10:54:39 -0700

    fixes getMatches using promises, update middleware to match production

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -2,31 +2,37 @@ module.exports = function(app) {
   // Install a "/ping" route that returns "pong"
   app.get("/getMatches", function(req, res) {
     var Account = app.models.Account;
-    var there = {
+    var there = new app.loopback.GeoPoint({
       lat: parseFloat(req.query.lat),
       lng: parseFloat(req.query.lng)
-    };
-    return res.json(
-      Account.find(
-        {
-          where: {
-            location: {
-              near: there,
-              maxDistance: 2000
-            }
-          }
-        },
-        function(err, accounts) {
-          for (let index = 0; index < accounts.length; index++) {
-            accounts[index].distance = accounts[
-              index
-            ].location.distanceTo(there, {
-              type: "miles"
-            });
-          }
-          return accounts;
+    });
+
+    let Nearby = Account.find({
+      fields: {
+        id: true,
+        name: true,
+        location: true
+      },
+      where: {
+        location: {
+          near: there,
+          maxDistance: 2000
         }
-      )
-    );
+      }
+    })
+      .then(data => {
+        for (var account in data) {
+          data[account].distance = new app.loopback.GeoPoint(
+            data[account].location
+          ).distanceTo(there, {
+            type: "meters"
+          });
+          delete data[account].location;
+        }
+        return res.json(data);
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
   });
 };
