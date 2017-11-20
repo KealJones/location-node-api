108d01ef0625072d8cd52e3fab3a8c4f097a472c
KealJones <kealjones@gmail.com>
2017-11-20 01:04:08 -0700

    update automigration file

--- bin/automigrate.js
+++ bin/automigrate.js
@@ -37,3 +37,14 @@ ds.automigrate("Account", function(err) {
     });
   });
 });
+
+var server = app;
+var lbTables = ["User", "AccessToken", "ACL", "RoleMapping", "Role"];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log(
+    "Loopback tables [" - lbTables - "] created in ",
+    ds.adapter.name
+  );
+  ds.disconnect();
+});
