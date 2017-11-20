fd3622aca6429238887da28be7cc809c0d9318f6
KealJones <kealjones@gmail.com>
2017-11-20 11:43:38 -0700

    moves matching logic and manipulation of Accounts to the model for a cleaner route and seperation of concerns

--- common/models/account.js
+++ common/models/account.js
@@ -1,6 +1,7 @@
+const loopback = require("loopback");
 module.exports = function(Account) {
   Account.observe("before save", function(ctx, next) {
-    var now = new Date();
+    let now = new Date();
     if (ctx.isNewInstance) {
       ctx.instance.createdAt = now;
       ctx.instance.lastModifiedAt = now;
@@ -9,5 +10,50 @@ module.exports = function(Account) {
     }
 
     next();
+  });
+
+  Account.getByLocation = (latitude, longitude, role) => {
+    let there = new loopback.GeoPoint({
+      lat: parseFloat(latitude),
+      lng: parseFloat(longitude)
+    });
+    let searchCriteria = {
+      fields: {
+        id: true,
+        name: true,
+        location: true
+      },
+      where: {
+        location: {
+          near: there
+        }
+      }
+    };
+    if (role) searchCriteria.where.role = role;
+
+    return Account.find(searchCriteria)
+      .then(data => {
+        for (var account in data) {
+          data[account].distance = new loopback.GeoPoint(
+            data[account].location
+          ).distanceTo(there, {
+            type: "meters"
+          });
+          delete data[account].location;
+        }
+        return data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  Account.remoteMethod("getByLocation", {
+    accepts: [
+      { arg: "latitude", type: "number", required: true },
+      { arg: "longitude", type: "number", required: true },
+      { arg: "role", type: "string" }
+    ],
+    returns: { type: "array" }
   });
 };
--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,38 +1,42 @@
 module.exports = function(app) {
-  // Install a "/ping" route that returns "pong"
-  app.get("/getMatches", function(req, res) {
-    var Account = app.models.Account;
-    var there = new app.loopback.GeoPoint({
-      lat: parseFloat(req.query.lat),
-      lng: parseFloat(req.query.lng)
-    });
+  /*
+Expected URL:
+https://hairdoo.co/getMatches?latitude=24.421&longitude=127.391
 
-    let Nearby = Account.find({
-      fields: {
-        id: true,
-        name: true,
-        location: true
-      },
-      where: {
-        location: {
-          near: there,
-          maxDistance: 2000
-        }
-      }
-    })
-      .then(data => {
-        for (var account in data) {
-          data[account].distance = new app.loopback.GeoPoint(
-            data[account].location
-          ).distanceTo(there, {
-            type: "meters"
-          });
-          delete data[account].location;
-        }
-        return res.json(data);
-      })
-      .catch(function(error) {
-        console.error(error);
+Expected Example Response:
+
+[{
+	'Name': "John Artist 1",
+  'Distance': 512.74
+}, {
+	'Name': "Ted Artist 2",
+	'Distance': 953.2
+}, {
+	'Name': "Vince Artist 3",
+	'Distance': 1329.68
+}]
+
+  *
+  */
+  app.get("/getMatches", (req, res) => {
+    let Account = app.models.Account;
+    if (!req.query.latitude)
+      return res.status(400).send({
+        status: 400,
+        message: "parameter `latitude` is required."
       });
+    if (!req.query.longitude)
+      return res.status(400).send({
+        status: 400,
+        message: "parameter `longitude` is required."
+      });
+
+    return Account.getByLocation(
+      req.query.latitude,
+      req.query.longitude,
+      "artist"
+    )
+      .then(data => res.json(data))
+      .catch(err => console.error(err));
   });
 };
